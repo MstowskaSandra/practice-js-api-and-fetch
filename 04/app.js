7a4c2c95a67d03e0f6e784a2a32c7557c75c60d4
Sandra <sandra.mstowskaa@gmail.com>
2025-09-30 19:12:59 +0200

    task04: Add inputs support

--- 04/app.js
+++ 04/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector('form').addEventListener('submit', function(event) {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
         event.preventDefault();
         const inputLatitude = document.querySelector('.form__field--lat');
         const inputLongitude = document.querySelector('.form__field--lng');
-        latitude = inputLatitude.value;
-        longitude = inputLongitude.value;
+        latitude = inputLatitude.value.trim();
+        longitude = inputLongitude.value.trim();
         init();
     });
 });
@@ -14,6 +14,11 @@ let apiData;
 let latitude;
 let longitude;
 const key = 'f5662bc59e904427ae9be3b4822d89fe';
+const weatherLat = document.querySelector('.weather__lat');
+const weatherLng = document.querySelector('.weather__lng');
+const weatherSummary = document.querySelector('.weather__summary');
+const weatherTemp = document.querySelector('.weather__temperature');
+
 
 function init() {
     console.log('DOM');
@@ -29,12 +34,17 @@ function init() {
         .then(ip => {
             apiData = ip;
             console.log("API", apiData);
+            weatherLat.innerText = latitude;
+            weatherLng.innerText = longitude;
+            weatherSummary.innerText = apiData.data[0].weather.description;
+            weatherTemp.innerText = apiData.data[0].temp;
         })
         .catch(err => console.error(err))
         .finally(() => {
             console.log('API ok')
         });
 }
+
 
 
 
